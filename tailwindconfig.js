aed3eb89b64cb646c2a26479b6ac63b680d55ec1
Orcun Elektirikci <orcunelektirikci@gmail.com>
2024-04-12 15:58:23 +0300

    completed

---
+++ tailwindconfig.js
@@ -0,0 +1,17 @@
+   tailwind.config = {
+      theme: {
+        fontFamily: {
+          body: 'Outfit, sans-serif'
+        },
+        extend: {
+          colors: {
+            primary: '#2C7DFA',
+            shade: '#3685FF',
+            darkNavy: '#1F314F',
+            gray: '#7D889E',
+            lightGray: '#D5E1EF',
+            white: '#fff'
+          }
+        }
+      }
+    }
